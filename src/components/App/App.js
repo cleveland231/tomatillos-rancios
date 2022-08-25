4359757a07e78c2c7ad068b263ba96144d3e06f4
Cole Stephenson <colestephenson94@gmail.com>
2022-08-25 15:31:58 -0600

    Creates component structure for all needed components and associated styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import movieData from './movieData';
 // import singleMovie from '../../singleMovie';
 import './App.css'
 import MoviesBox from '../MoviesBox/MoviesBox';
+import '../MoviesBox/MoviesBox.css'
 
 class App extends Component {
     constructor() {
@@ -13,14 +14,10 @@ class App extends Component {
     }
 
     render() {
-        // console.log('tsm', this.state.movies)
-
         return (
             <div className='main'>
                 <h1> tomatillos rancios </h1>
-                <MoviesBox
-                    movies={this.state.movies} 
-                />
+                <MoviesBox movies={this.state.movies} />
             </div>
         )
     }
