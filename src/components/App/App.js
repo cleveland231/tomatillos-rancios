34acbe1b67f3ab31f6ff3d35a681f1a02b9df7e0
Cleveland Ticoalu <ayocleveland@gmail.com>
2022-08-26 19:57:31 -0600

    Add functionality to home button and api calls

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,27 +4,41 @@ import SingleMovie from '../SingleMovie/SingleMovie.js';
 import './App.css'
 import MoviesBox from '../MoviesBox/MoviesBox';
 import '../MoviesBox/MoviesBox.css'
+import fetchApiData from './api-calls';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            movies: movieData,
+            movies: [],
             singleMovieView: false,
             selectedMovie: null,
         }
     }
 
+    
+    componentDidMount = () => {
+        fetchApiData()
+            .then(data => {
+            this.setState({movies: data.movies})
+            })
+            .catch(err => console.log(err))
+    }
+
     testMethod = event => {
       event.preventDefault();
       let parsedID = parseInt(event.target.id);
-      let selectedMovie = this.state.movies.movies.find(movie => movie.id === parsedID)
+      let selectedMovie = this.state.movies.find(movie => movie.id === parsedID)
 
-      this.setState({ movies: movieData,
+      this.setState({ movies: movieData.movies,
       singleMovieView: true,
       selectedMovie: selectedMovie })
+    }
 
-
+    goHome = (event) => {
+        event.preventDefault();
+        this.setState({ movies: movieData.movies,
+        singleMovieView: false })
     }
 
     render() {
@@ -32,7 +46,7 @@ class App extends Component {
             <div className='main'>
                 <h1> 🍅 Bienvenidos a Tomatillos Rancios! 🍅 </h1>
                 { !this.state.singleMovieView  && <MoviesBox movies={this.state.movies} method={this.testMethod}/>}
-                {this.state.singleMovieView && <SingleMovie movie={this.state.selectedMovie} />}
+                {this.state.singleMovieView && <SingleMovie movie={this.state.selectedMovie} method={this.goHome} />}
             </div>
         )
     }
