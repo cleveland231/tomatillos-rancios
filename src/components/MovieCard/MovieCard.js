fd2de57f15184fbd070defffae0c529f81c3daf9
Cole Stephenson <colestephenson94@gmail.com>
2022-08-29 12:39:22 -0600

    Implement Router

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './MovieCard.css';
+import {NavLink} from 'react-router-dom'
 
 const movieCard = ({poster_path, title, method, id}) => {
     return (
+      <NavLink to={`movies/${id}`}>
         <div className='single-movie'>
-            <img id={id} className='poster' alt={title} src={poster_path} onClick={event => method(event)} />
+            <img id={id} className='poster' alt={title} src={poster_path}/>
             <h3>{title}</h3>
         </div>
+      </NavLink>
     )
 }
 
